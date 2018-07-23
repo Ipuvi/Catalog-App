4b781b81677ba814b38637d4f1f068d1ab23e114
Ipuvi Mishra <ipuvi98@gmail.com>
2018-07-23 12:21:42 +0530

    Modifies database setup

--- databasetup.py
+++ databasetup.py
@@ -11,7 +11,7 @@ Base = declarative_base()
 class User(Base):
     """docstring for User"""
     __tablename__ = 'user'
-    
+
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
     email = Column(String(250), nullable=False)
@@ -24,7 +24,7 @@ class User(Base):
         'id' : self.id,
         'name' : self.name,
         'email' : self.email,
-        'picture' : self.picture,
+        'picture' : self.picture
         }
 
 
@@ -49,8 +49,8 @@ class Category(Base):
 
 
 # Restaurant Class ( Category 1)..... 
-class Restaurant(Base):
-    __tablename__ = 'restaurant'
+class CategoryList(Base):
+    __tablename__ = 'list'
    
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
@@ -70,16 +70,15 @@ class Restaurant(Base):
         }
 
 # Further sub category for Restaurant class...
-class MenuItem(Base):
-    __tablename__ = 'menu_item'
+class Items(Base):
+    __tablename__ = 'items'
 
     name =Column(String(80), nullable = False)
     id = Column(Integer, primary_key = True)
     description = Column(String(250))
     price = Column(String(8))
-    course = Column(String(250))
-    restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
-    restaurant = relationship(Restaurant) 
+    list_id = Column(Integer, ForeignKey('list.id'))
+    list = relationship(CategoryList)
     user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship(User)
 
@@ -91,109 +90,9 @@ class MenuItem(Base):
         'description' : self.description,
         'id' : self.id,
         'price' : self.price,
-        'course' : self.course,
-        'user_id' : self.user_id,
-        }
- 
-# Hotel Class ( Category 2 ).....
-class Hotel(Base):
-    __tablename__ = 'hotel'
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-    category_id = Column(Integer, ForeignKey('category.id'))
-    user_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship(User)
-    category = relationship(Category)
-
-    @property
-    def serialize(self):
-        #Returns Object data in easily serialiazable form...
-        return {
-        'name' : self.name,
-        'id' : self.id,
-        'user_id' : self.user_id,
-        'category_id' : self.category_id
-        }
-
-#Further sub category for Hotel Class...
-class HotelDetails(Base):
-    """docstring for HotelList"""
-    __tablename__ = 'hoteldetails'
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-    description = Column(String(250))
-    price = Column(String(8))
-    room_type = Column(String(250))
-    hotel_id = Column(Integer, ForeignKey('hotel.id'))
-    hotel = relationship(Hotel)
-    user_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship(User)
-
-    @property
-    def serialize(self):
-        #Returns Object data in easily serialiazable form...
-        return {
-        'name' : self.name,
-        'description' : self.description,
-        'id' : self.id,
-        'price' : self.price,
-        'room_type' : self.room_type,
-        'hotel_id' : self.hotel_id,
         'user_id' : self.user_id
         }
  
-
-#Destination Class ( Category 3 ).....
-class Destination(Base):
-    """docstring for Destinations"""
-    __tablename__ = 'destination' 
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-    category_id = Column(Integer, ForeignKey('category.id'))
-    category = relationship(Category)
-    user_id = Column(Integer, ForeignKey('user.id'))
-    user =relationship(User)
-
-    @property
-    def serialize(self):
-        #Return Object data in easily serialiazable form...
-        return {
-        'name' : self.name,
-        'id' : self.id,
-        'user_id' : self.user_id,
-        'category_id' : self.category_id
-        }
-        
-
-#Further sub category for School Class...
-class DestinationDetails(Base):
-    """docstring for SchoolDetails"""
-    __tablename__ = 'destinationdetails'
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-    description = Column(String(250))
-    address = Column(String(250))
-    destination_id = Column(Integer, ForeignKey('destination.id'))
-    destination = relationship(Destination)
-    user_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship(User)
-    
-    @property
-    def serialize(self):
-        #Returns Object data in easily serialiazable form...
-        return {
-        'name' : self.name,
-        'id' : self.id,
-        'description' : self.description,
-        'address' : self.address,
-        'destination_id' : self.school_id,
-        'user_id' : self.user_id
-        }
-    
 
 engine = create_engine('sqlite:///Catalog.db')
 Base.metadata.create_all(engine)
